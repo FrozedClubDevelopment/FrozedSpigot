92949fcacc15bba6580d4a8290f391e62965785c
Elb1to <elbitomc@gmail.com>
2020-11-23 21:22:10 -0500

    Reformatted commands

--- NachoSpigot-API/src/main/java/org/bukkit/command/defaults/nacho/TestTpCommand.java
+++ NachoSpigot-API/src/main/java/org/bukkit/command/defaults/nacho/TestTpCommand.java
@@ -2,16 +2,14 @@ package org.bukkit.command.defaults.nacho;
 
 
 import dev.cobblesword.nachospigot.CC;
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
 public class TestTpCommand extends Command {
-    public TestTpCommand(String name)
-    {
+
+    public TestTpCommand(String name) {
         super(name);
         this.description = "teleport test mobs";
         this.usageMessage = "/testtp";
@@ -19,21 +17,19 @@ public class TestTpCommand extends Command {
     }
 
     public boolean execute(CommandSender sender, String commandLabel, String[] args) {
-        if (!testPermission(sender))
-            return true;
+        if (!testPermission(sender)) return true;
 
-        if(!(sender instanceof Player))
-        {
-            sender.sendMessage(CC.red + "Please run this command as a player");
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(CC.RED + "Please run this command as a player");
             return true;
         }
 
         Player player = (Player) sender;
         Location location = player.getLocation();
 
-
         player.teleport(new Location(location.getWorld(), 10, 80, 12));
-        player.sendMessage(CC.green + "Teleported...");
+        player.sendMessage(CC.GREEN + "Teleported...");
+
         return false;
     }
 }

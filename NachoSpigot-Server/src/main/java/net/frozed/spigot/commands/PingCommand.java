9fbe3af7bc52ee5869c1b8817e6b9db481d703bc
Elb1to <elbitomc@gmail.com>
2020-11-24 17:06:25 -0500

    Coded Ping Command

--- NachoSpigot-Server/src/main/java/net/frozed/spigot/commands/PingCommand.java
+++ NachoSpigot-Server/src/main/java/net/frozed/spigot/commands/PingCommand.java
@@ -1,6 +1,42 @@
-package net.frozed.spigot.commands;/**
+package net.frozed.spigot.commands;
+
+import dev.cobblesword.nachospigot.CC;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
+
+/**
  * Created by Elb1to
  * Project: FrozedSpigot
  * Date: 11/24/2020 @ 3:00 PM
- */public class PingCommand {
+ */
+public class PingCommand extends Command {
+
+    public PingCommand() {
+        super("ping");
+        this.usageMessage = "/ping <player>";
+        this.setAliases(Arrays.asList("latency", "playerconnection"));
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String label, String[] args) {
+        Player player = (Player) sender;
+        if (args.length < 1) {
+            player.sendMessage(CC.translate("&bYour ping: &f" + ((CraftPlayer) player).getHandle().ping + "ms"));
+            return true;
+        }
+
+        Player target = Bukkit.getPlayer(args[0]);
+        if (target != null) {
+            player.sendMessage(CC.translate("&b" + target.getName() + "'s ping: &f" + ((CraftPlayer) target).getHandle().ping + "ms"));
+        } else {
+            player.sendMessage(CC.translate("&c" + args[0] + " is not online."));
+        }
+
+        return true;
+    }
 }

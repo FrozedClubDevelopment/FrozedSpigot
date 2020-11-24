cc255919a838d8266b4a78ad804f8bc410487006
Elb1to <elbitomc@gmail.com>
2020-11-24 17:08:46 -0500

    /KillEntities and /Entities [Commands]
    They both require debugging and fixing xd

--- NachoSpigot-Server/src/main/java/net/frozed/spigot/commands/EntitiesCommand.java
+++ NachoSpigot-Server/src/main/java/net/frozed/spigot/commands/EntitiesCommand.java
@@ -1,6 +1,47 @@
-package net.frozed.spigot.commands;/**
+package net.frozed.spigot.commands;
+
+import dev.cobblesword.nachospigot.CC;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
+
+/**
  * Created by Elb1to
  * Project: FrozedSpigot
  * Date: 11/24/2020 @ 3:06 PM
- */public class EntitiesCommand {
+ */
+public class EntitiesCommand extends Command {
+
+    public EntitiesCommand() {
+        super("entities");
+        this.usageMessage = "/entities";
+        this.setAliases(Arrays.asList("getmobs", "allmobs", "mobslist"));
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String label, String[] args) {
+        Player player = (Player) sender;
+
+        if (player.hasPermission("frozedspigot.command.entities")) {
+            player.sendMessage(CC.translate("&aGetting all alive entities and classifying them..."));
+
+            for (World world : Bukkit.getWorlds()) {
+                for (Entity entity : world.getEntities()) {
+                    // player.sendMessage(CC.translate("&8 ● &b(&f" + entity.getType().name() + "&b)"));
+                    //player.sendMessage(CC.translate("&8 ● &b(&f" + entity.getType().name() + "&b) &8[&7x" + entity.getWorld().getEntities().size() + "&8]"));
+                }
+                return true;
+            }
+        } else {
+            player.sendMessage(CC.translate("&cYou don't have permission to do this."));
+            return true;
+        }
+
+        return true;
+    }
 }
--- NachoSpigot-Server/src/main/java/net/frozed/spigot/commands/KillEntitiesCommand.java
+++ NachoSpigot-Server/src/main/java/net/frozed/spigot/commands/KillEntitiesCommand.java
@@ -1,6 +1,52 @@
-package net.frozed.spigot.commands;/**
+package net.frozed.spigot.commands;
+
+import dev.cobblesword.nachospigot.CC;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
+
+/**
  * Created by Elb1to
  * Project: FrozedSpigot
  * Date: 11/24/2020 @ 3:00 PM
- */public class KillEntitiesCommand {
+ */
+public class KillEntitiesCommand extends Command {
+
+    public KillEntitiesCommand() {
+        super("killentities");
+        this.usageMessage = "/killentities";
+        this.setAliases(Arrays.asList("removeentities", "removemobs", "killmobs"));
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String label, String[] args) {
+        Player player = (Player) sender;
+
+        /*
+         * For some reason the executor of the command gets his client frozen
+         * whenever he/she executes the /killentities command...
+         */
+        if (player.hasPermission("frozedspigot.command.killentities")) {
+            player.sendMessage(CC.translate("&aRemoving all entities..."));
+
+            int entitiesRemoved = 0;
+            for (World world : Bukkit.getWorlds()) {
+                for (Entity entity : world.getEntities()) {
+                    entity.remove();
+                    entitiesRemoved++;
+                }
+            }
+            player.sendMessage(CC.translate("&a" + entitiesRemoved + " entities were removed."));
+        } else {
+            player.sendMessage(CC.translate("&cYou don't have permission to do this."));
+            return true;
+        }
+
+        return true;
+    }
 }
